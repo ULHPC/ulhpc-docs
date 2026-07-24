5ff906b31c408624cc5d21f8f569b87d78f2d961
Jorge Delgado Guerrero <jorge.delgadoguerrero@uni.lu>
2026-07-24 18:01:01 +0200

    [Build&Refactor] Full refactor and implementation of new functions

--- scripts/eb_metadata.py
+++ scripts/eb_metadata.py
@@ -1,169 +1,200 @@
+import json
+import socket
+import os
+from pathlib import Path
+
 from easybuild.tools.options import set_up_configuration
 from easybuild.framework.easyconfig.easyconfig import process_easyconfig
-from pathlib import Path
+from easybuild.framework.easyconfig.parser import EasyConfigParser
 import pandas as pd
-import json
 
+#-----------------------------------------
+# Run only with Easybuild versions 5.3.x or above
+# Define available software releases
 # Add only software in "release" modules! Only one build number in release.
-software = {
-    "aion": {
-        "epyc": {
-            "2026a": "rhel810-20260107",
-            "2025a": None,
-            "2024a": None,
-            "2023b": None,
-        },
-    },
-    "iris": {
-        "broadwell": {
-            "2026a": None,
-            "2025a": None,
-            "2024a": None,
-            "2023b": None,
-        },
-        "skylake": {
-            "2026a": None,
-            "2025a": None,
-            "2024a": None,
-            "2023b": None,
-        },
-        "gpu": {
-            "2026a": None,
-            "2025a": None,
-            "2024a": None,
-            "2023b": None,
-        },
-        "hopper": {
-            "2026a": None,
-            "2025a": None,
-            "2024a": None,
-            "2023b": None,
-        },
-    },
-}
-base_path = Path("/opt/apps/easybuild/systems/")
 
-def extract_easyconfigs_info(eb, software_path, stack):
+cluster_info = {
+    'releases' : [
+        "2025a",
+        "2024a",
+        "2023b",
+    ],
+
+    'build' : "rhel810-20260107",
+
+    'architectures' : {
+        'epyc' : "AMD EPYC",
+        'broadwell' : "Intel Broadwell",
+        'skylake' : "Intel Skylake",
+        'saphirerapids' : "Intel Sapphire Rapids",
+        'gpu' : "NVIDIA Tesla",
+    },
+
+    'base_path' : "/opt/apps/easybuild/systems/"
+}
+
+#-----------------------------------------
+
+###############################
+## EXTRACT EASYCONFIGS INFO ##
+###############################
+
+def get_software_stack(software_path, hwd_data):
+    stack = []
+
+    for eb in software_path.rglob("*.eb"):
+        if "reprod" in eb.parts:
+            continue
+        
+        print(eb)
+        extract_easyconfigs_info(eb, software_path, stack, hwd_data)
+
+    return stack
+
+
+def extract_easyconfigs_info(eb, software_path, stack, hwd_data):
     if eb.is_relative_to(software_path / "EasyBuild"):
         return stack
 
     try:
-        ecs = process_easyconfig(str(eb), validate = False)
+        parser = EasyConfigParser(filename = str(eb))
+        ec = parser.get_config_dict(validate = False)
 
-        for ecdict in ecs:
-            ec = ecdict["ec"]
+        package = {
+            "Name": ec.get("name"),
+            "Version": ec.get("version"),
+            "Homepage": ec.get("homepage"),
+            "Architectures": cluster_info["architectures"][hwd_data[1]],
+            "Clusters" : hwd_data[0],
+            "Description": ec.get("description"),
+            "Category": ec.get("moduleclass"),
+        }
 
-            package = {
-                "name": ec["name"],
-                "version": ec["version"],
-                "homepage": ec["homepage"],
-                "description": ec["description"],
-                "moduleclass": ec["moduleclass"],
-            }
-
-            stack.append(package)
+        stack.append(package)
 
     except Exception as excpt:
         print(f"Failed: {eb}: {excpt}")
 
     return stack
 
-def get_software_stack(software_path):
-    stack = []
-    options, cfg_settings = set_up_configuration(args=[])
+#-----------------------------------------
 
-    for eb in software.rglob("*.eb"):
-        #print(eb)
-        extract_easyconfigs_info(eb, software_path, stack)
-
-    return stack
-
-
-# with open("stack.json", "w") as fp:
-#     json.dump(stack, fp, indent=2, default=str)
-
-#stack = pd.read_json("stack.json")
+##########################
+## CLEAN AND MERGE DATA ##
+##########################
 
 def homepage_link(text, url):
     if pd.notna(url) and url:
         return f"[{text}]({url})"
     return text
 
-def get_softare_table(stack):
+def get_software_table(stack):
     df = pd.DataFrame(stack)
 
     # Clean software descriptions
-    df["description"] = df["description"]
+    df["Description"] = (df["Description"]
         .fillna("")
         .str.replace(r"\s+", " ", regex=True)
-        .str.strip()
-
+        .str.strip())
+        
     # Combine versions for the same software
     df = df.groupby(
-            ["name", "homepage", "moduleclass", "description"],
+            ["Name",
+             "Homepage",
+             "Architectures",
+             "Clusters",
+             "Category",
+             "Description"],
             as_index=False
         ).agg({
-            "version": ( lambda x: "<br>".join(sorted(list(set(x)))) ),
+            "Version": ( lambda x: "<br>".join(sorted(list(set(x)))) ),
         })
 
     # Create Markdown hyperlinks
-    df["software"] = df.apply(
-        lambda row: homepage_link(row["name"], row["homepage"]),
+    df["Software"] = df.apply(
+        lambda row: homepage_link(row["Name"], row["Homepage"]),
         axis=1,
     )
+    
+    return df
 
-    # Reorder columns
-    df = df[
+def merge_dataframes(stack, collapse_descr = True):
+    # Take all df corresponding to the same release
+    df = pd.concat(stack, ignore_index=True)
+
+    # Group software available in different clusters and arch
+    merged_result = (
+        df.groupby(
+            ["Software", "Version", "Category", "Description"],
+            as_index=False,
+        )
+        .agg({
+            "Architectures": lambda s:", ". join(sorted(set(s))),
+            "Clusters": lambda s:", ". join(sorted(set(s))),
+        })
+    )
+
+    # Reorder data for Markdown table
+    merged_result = merged_result[
         [
-            "software",
-            "version",
-            "moduleclass",
-            "description",
+            "Software",
+            "Version",
+            "Architectures",
+            "Clusters",
+            "Category",
+            "Description",
         ]
     ]
 
-    # Sort alphabetically
-    df = df.sort_values(["software", "version"])
+    if collapse_descr == True:
+        merged_result["Description"] = merged_result["Description"].apply(
+            lambda d: (
+                "<details>"
+                "<summary>Show</summary>"
+                f"{d}"
+                "</details>"
+            )
+        )
 
-    return df
+    return merged_result
 
-def set_nested_map(mp, entries, value):
-    if len(entries) == 0:
-        return dict()
 
-    entry = entries.pop(0)
-    if len(entries) == 0:
-        mp[entry] = value
-        return mp
+#-----------------------------------------
 
-    if len(mp) == 0:
-        mp[entry] = dict()
+#####################
+## MAIN FUNCTIONS ###
+#####################
 
-    val = setup_map(mp[entry], entries, value)
-    mp[entry] = val
+def get_sotfware_tables(save_data = True):
+    clusters = os.listdir(cluster_info['base_path'])
+    clusters.remove('binary')
 
-    return mp
+    stack = {}
+    for rel in cluster_info['releases']:
+        stack[rel] = []
+        for cl in clusters:
+            hwd_data = [cl, None]
+            archs = os.listdir(str(Path(cluster_info['base_path']) / cl / cluster_info['build'] / rel))
+            for arch in archs:
+                hwd_data[1] = arch
+                full_path = f"{cluster_info['base_path']}{cl}/{cluster_info['build']}/{rel}/{arch}/software/"
+                tmp_stack = get_software_stack(Path(full_path), hwd_data)
+                stack[rel].append(get_software_table(tmp_stack))
+        result = merge_dataframes(stack[rel])
 
-def get_software_tables():
-    software_tables = dict()
-
-    for machine in software.keys():
-        for arch in software[machine].keys():
-            for version in software[machine][arch].keys():
-                if software[machine][arch][version] is not None:
-                    build_number = software[machine][arch][version]
-                    software_path = base_path / machine / arch / version / build_number / "software"
-                    stack = get_software_stack(software_path)
-                    table = get_softare_table(stack)
-
-                    # One build per (machine, arch, version) in release!
-                    software_tables = set_nested_map(software_tables, [machine, arch, version], table)
-
-    return software_tables
-
-## Write Markdown
-#with open("stack.md", "w") as f:
-#    f.write(df.to_markdown(index=False))
+        if save_data == True:
+            with open(f"{rel}.md", "w") as f:
+                f.write(result.to_markdown(index = False))
 
 ## Keep JSON too
 #df.to_json("stack.json", orient="records", indent=2)
+
+#-----------------------------------------
+
+def main():
+    options, cfg_settings = set_up_configuration(args=[])
+    get_sotfware_tables()
+                        
+if __name__ == "__main__":
+    main()
+
